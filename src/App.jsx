0f9c849a692747c2e706f602a882a216f860f0dc
Fawad ALi <fawadalichaudhary97@gmail.com>
2026-05-14 18:24:15 +0500

    routes mode change

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,31 @@
-import { Route, Routes } from "react-router-dom"
+import { createBrowserRouter, RouterProvider } from "react-router-dom"
 import MainPage from "./pages/MainPage"
 import AddQuestionPage from "./pages/AddQuestionPage"
 import QuizPage from "./pages/QuizPage"
 import ResultsPage from "./pages/ResultsPage"
 
 function App() {
+  const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <MainPage />
+    },
+    {
+      path: "/addquestions",
+      element: <AddQuestionPage />
+    },
+    {
+      path: "/quizpage",
+      element: <QuizPage />
+    },
+    {
+      path: "/resultspage",
+      element: <ResultsPage />
+    },
+  ])
   return (
     <>
-      <Routes>
-        <Route path="/" element={<MainPage />} />
-        <Route path="/addquestions" element={<AddQuestionPage />} />
-        <Route path="/quizpage" element={<QuizPage />} />
-        <Route path="/resultspage" element={<ResultsPage />} />
-      </Routes>
+      <RouterProvider router={router} />
     </>
   )
 }
